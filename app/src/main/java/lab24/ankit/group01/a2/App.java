a226b86c43f8ee692030be5b9b6a9a531f83c102
Joshua <jhal4273@uni.sydney.edu.au>
2023-10-15 19:20:31 +1100

    changed implementation slightly of menu and app classes

--- app/src/main/java/lab24/ankit/group01/a2/App.java
+++ app/src/main/java/lab24/ankit/group01/a2/App.java
@@ -12,49 +12,21 @@ public class App {
     public int selection;
 
     public App () {
-        menu = new Menu();
-        run();
-        menuIntro();
-        menu.menuSelection(menuDisplay(),user.getType());
+        // displaying login screen
+        Login login = new Login();
+        login.displayLoginScreen();
+        user = login.getUser();
+
+        // displaying menu
+        Menu menu = new Menu();
+        menu.menuIntro(user);
+        int selection = menu.showMenu(user);
+        menu.menuSelection(selection, user);
     }
 
     public static void main(String[] args) {
         // running the App
         new App();
     }
-
-    public void run() {
-        // displaying login screen
-        Login login = new Login();
-        login.displayLoginScreen();
-        user = login.getUser();
-
-    }
-
-    public void menuIntro(){
-        // Menu intro
-        if(user != null){
-            user.showUser();
-            menu.menuIntro(user.getName());
-            return;
-        }else{
-            System.out.println("\nUser: Guest");
-            menu.menuIntro("Guest");
-            return;
-        }
-    }
-
-    public int menuDisplay(){
-        if(user != null){
-            if(user.isAdmin() == true){
-                return menu.showMenu("Admin");
-            }else{
-                return menu.showMenu("Member");
-            }
-        }else{
-            return menu.showMenu("Guest");
-        }
-     }
-
 
 }
--- app/src/main/java/lab24/ankit/group01/a2/Menu.java
+++ app/src/main/java/lab24/ankit/group01/a2/Menu.java
@@ -4,49 +4,48 @@ import lab24.ankit.group01.a2.Scan;
 import java.util.Scanner;
 import java.util.List;
 
+import lab24.ankit.group01.a2.User_types.User;
+
 public class Menu{
 
-    public Scanner scan;
-    public Scan inputCheck;
-
-    public Menu(){
-        this.scan = new Scanner(System.in);
-    }
-
-    public void menuIntro(String name){
+    public void menuIntro(User user){
+        String name = "Guest";
+        if (user != null){
+            name = user.getName();
+        }
         System.out.println("Hello "+ name +" I am then (VSAS) Virtual Scroll Access System.");
         System.out.println("The portal to the realm of digital wisdom!");
         System.out.println("I await your selection:");
     }
 
-    public int showMenu(String userType){
+    public int showMenu(User user){
         //Menu display
         while (true){
-            if(userType.equals("Guest")){
+            if(user == null){
                 System.out.println("1. View Scrolls\n2. Exit");
-                int selection = inputCheck.scanInteger(1, 2);
+                int selection = Scan.scanInteger(1, 2);
                 if (selection == 2){
                     System.out.println("Good luck anonymous member of the Whiskers!");
                     System.exit(0);
                 }else{
                     return selection;
                 }
-            }else if(userType.equals("Member")){
+            }else if(!user.isAdmin()){
                 System.out.println("1. View Scrolls\n2. Add Scrolls\n3. Edit Scrolls\n4. Recieve Scrolls\n5. Search Scrolls\n6. Change username\n"
                                 +"7. Change password\n8. Exit");
-                int selection = inputCheck.scanInteger(1, 8);
+                int selection = Scan.scanInteger(1, 8);
                 if (selection == 8){
                     System.out.print("Good luck member of the Whiskers!");
                     System.exit(0);
                 }else{
                     return selection;
                 }
-            }else if (userType.equals("Admin")){
+            }else if (user.isAdmin()){
                                 System.out.println("1. View Scrolls\n2. Add Scrolls\n"
                                 +"3. Edit Scrolls\n4. Recieve Scrolls\n5. Search Scrolls\n6. " 
                                 +"Create new Users\n7. View users\n8. Delete users\n9. View App stats\n 10. Change username\n"
                                 +"11. Change password\n12. Exit");
-                int selection = inputCheck.scanInteger(1, 12);
+                int selection = Scan.scanInteger(1, 12);
                 if (selection == 12){
                     System.out.print("Good luck Admin of the Whiskers!");
                     System.exit(0);
@@ -57,8 +56,13 @@ public class Menu{
         }
     }
 
-    public void menuSelection(int selection, String name){
-        if(name.equals("member")){
+    public void menuSelection(int selection, User user){
+        if (user == null && selection != 1){
+            System.out.println("You must be logged in to do that!");
+            return;
+        }
+
+        if(user != null && !user.isAdmin()){
             if(selection == 6){
                 selection = 10;
             }else if (selection == 7){
